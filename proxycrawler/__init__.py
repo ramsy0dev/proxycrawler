25e120ff51eb1858360ab661d86c2ff411816e2f
ramsy0dev <ramsy0dev@gmail.com>
2024-04-12 06:43:14 +0000

    Export modules and classes

--- proxycrawler/__init__.py
+++ proxycrawler/__init__.py
@@ -1,3 +1,15 @@
+
+# Copyright (C) 2023-2024 the proxycrawler author
+# This module if part of proxycrawler and is released under
+# the GPL-3.0 License: <see LICENSE file>
+
+# Models
+from .src.models.proxy_model import ProxyModel
+
+# Proxycrawler
 from .src.proxycrawler import ProxyCrawler
-from .src.services.freeproxylist import FreeProxyList
-from .src.services.geonode import Geonode
+
+# Package info
+__author__ = "ramsy0dev"
+__package__ = "proxycrawler"
+__version__ = "0.2.6"
